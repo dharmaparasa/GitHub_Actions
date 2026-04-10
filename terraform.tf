52f06b097003e639711b6738db76a4c4d420e7e1
Dharma <dharmaparasa@yahoo.com>
2026-04-10 07:14:15 +0530

    c

--- terraform.tf
+++ terraform.tf
@@ -2,37 +2,37 @@
 #Create a keypair in same region as in provider
 #change subnet, vpcid and keypair name in the code
 
-resource "aws_security_group" "name" {
-    # name = "DharmaSG"
-    tags = {
-      Name="dsg"
-    }
-    vpc_id = "vpc-0a0653a840184508e"
-    dynamic "ingress" {
-      for_each = [
-        for port,cidr in {
-          22 = "0.0.0.0/0" }:{
-          from_port=port
-          to_port=port
-          cidr_blocks=[cidr]
-          protocol="tcp"
-        }
-      ]
-      content {
-          from_port   = ingress.value.from_port
-          to_port     = ingress.value.to_port
-          protocol    = ingress.value.protocol
-          cidr_blocks = ingress.value.cidr_blocks
-      }
-    }
+# resource "aws_security_group" "name" {
+#     # name = "DharmaSG"
+#     tags = {
+#       Name="dsg"
+#     }
+#     vpc_id = "vpc-0a0653a840184508e"
+#     dynamic "ingress" {
+#       for_each = [
+#         for port,cidr in {
+#           22 = "0.0.0.0/0" }:{
+#           from_port=port
+#           to_port=port
+#           cidr_blocks=[cidr]
+#           protocol="tcp"
+#         }
+#       ]
+#       content {
+#           from_port   = ingress.value.from_port
+#           to_port     = ingress.value.to_port
+#           protocol    = ingress.value.protocol
+#           cidr_blocks = ingress.value.cidr_blocks
+#       }
+#     }
 
-    egress {
-        from_port = 0
-        to_port= 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
+#     egress {
+#         from_port = 0
+#         to_port= 0
+#         protocol = "-1"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+# }
 
 
 
@@ -41,15 +41,16 @@ resource "aws_security_group" "name" {
 
 resource "aws_instance" "name" {
     instance_type = "t3.micro"
-    ami = "ami-03caad32a158f72db"
+    # ami = "ami-03caad32a158f72db"  #Oregon
+    ami = "ami-02dfbd4ff395f2a1b"
     tags = {
       Name="Dharma"
     }
     key_name = "pdl"
     
     #mention for another region
-    subnet_id = "subnet-0b654b6b7c4b61498"
-    security_groups = [aws_security_group.name.id]
+    # subnet_id = "subnet-0b654b6b7c4b61498"
+    # security_groups = [aws_security_group.name.id]
     associate_public_ip_address = true
 }
 
